2efdf403badd6d8644edccde494ccc4b0d676564
JesusLopezMo <jesuslopezmore96@gmail.com>
2024-04-04 15:57:31 -0500

    nav y lectura fase 01

--- app/src/main/java/com/example/trvciuw/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/trvciuw/ui/home/HomeFragment.java
@@ -1,10 +1,18 @@
 package com.example.trvciuw.ui.home;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,23 +23,52 @@ import com.example.trvciuw.databinding.FragmentHomeBinding;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private NfcAdapter nfcAdapter;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.fechaActual;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(getContext());
+        if(nfcAdapter == null){
+            Toast.makeText(getContext(), "Este dispositivo no soporta NFC", Toast.LENGTH_SHORT).show();
+
+        }else if(!nfcAdapter.isEnabled()){
+            Toast.makeText(getContext(), "NFC está desactivado", Toast.LENGTH_SHORT).show();
+
+        }else{
+            Toast.makeText(getContext(), "NFC está activado", Toast.LENGTH_SHORT).show();
+        }
+        binding.linkTriveca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://linktr.ee/triveca"));
+                startActivity(browserIntent);
+            }
+        });
         return root;
     }
+
+
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/trvciuw/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/trvciuw/ui/home/HomeViewModel.java
@@ -1,19 +1,56 @@
 package com.example.trvciuw.ui.home;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.NdefRecord;
+import android.nfc.NdefMessage;
+
+import android.os.Parcelable;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class HomeViewModel extends ViewModel {
 
+    /*
     private final MutableLiveData<String> mText;
-
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }
-
     public LiveData<String> getText() {
         return mText;
-    }
+    }*/
+
+
+    private NfcAdapter nfcAdapter;
+    private PendingIntent pendingIntent;
+    private IntentFilter[] readingTagFilters;
+
+
+
+    // Lista para almacenar palabras que empiecen con "Vol"
+    private List<String> volWordsList;
+    String firstLine = null;
+
+
+
+
+
+
+
+
 }
